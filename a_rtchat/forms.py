ac761438aa812be90444093d5117bbd92974a78f
dilancroos <dilancroos@outlook.com>
2024-03-22 01:40:58 +0100

    Fin: Chat componant

---
+++ a_rtchat/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+
+class ChatmessageCreateForm(ModelForm):
+    class Meta:
+        model = GroupMessage
+        fields = ['body']
+        widgets = {
+            'body': forms.TextInput(attrs={'placeholder': 'Type a message...', 'class': 'p-4 text-black', 'maxlength': '300', 'autofocus': True}),
+        }
